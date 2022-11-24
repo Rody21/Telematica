e0b9e95ea9515a58501f48d8dd9a56fa7ec1b43c
Rody <mjguette21@gmail.com>
2022-11-24 18:53:13 -0500

    Desplegables

--- js/slideArbitros.js
+++ js/slideArbitros.js
@@ -2,4 +2,18 @@
     const titleAdmins = [...document.querySelectorAll('.Admin__title')];
     console.log(titleAdmins)
 
+    titleAdmins.forEach(Admin =>{
+        Admin.addEventListener('click',()=>{
+            let height = 0;
+            let answer = Admin.nextElementSibling;
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        });
+
+    });
+
 })();
