49b85fe81a99d2f6e16ab8c6aa242bb812bad37a
Rody <mjguette21@gmail.com>
2022-11-24 19:21:53 -0500

    slider arbitro 46%

--- js/slideArbitros.js
+++ js/slideArbitros.js
@@ -6,6 +6,11 @@
         Admin.addEventListener('click',()=>{
             let height = 0;
             let answer = Admin.nextElementSibling;
+            let addPadding = Admin.parentElement.parentElement;
+
+
+            addPadding.classList.toggle('Admin__padding--add');
+            Admin.children[0].classList.toggle('Admin__arrow--rotate');
 
             if(answer.clientHeight === 0){
                 height = answer.scrollHeight;
