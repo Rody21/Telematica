cc610b654843afee7c79f6ca6c054deac1593da6
Rody <mjguette21@gmail.com>
2022-11-26 10:00:54 -0500

    resultados slide 100%

--- js/Partido1.js
+++ js/Partido1.js
@@ -1,21 +1,86 @@
-function table(){
-    $.ajax({
-        url: "../Telematica/php/Partidos1.php", 
-        type: "GET",
-        dataType: "JSON",
-        data: {}, //this is data you send to your server
-        success: function(res) {
-          console.log(res);
-          console.log(res.length);
-      
-          for (i = 0; i < res.length; i++) {
-            $('#Partidos1').append(
-                '<tr><td>' + res[i]['Local'] +
-                '</td><td>' + res[i]['Visitante'] +
-                '</td></tr>')
-          }
-      
-        }
-      })
+function table() {
+  $.ajax({
+    url: "../Telematica/php/Partidos1.php",
+    type: "GET",
+    dataType: "JSON",
+    data: {}, //this is data you send to your server
+    success: function (res) {
+      console.log(res);
+      console.log(res.length);
+
+      for (i = 0; i < res.length; i++) {
+        $("#Partidos1").append(
+          "<tr><td>" +
+            res[i]["Local"] +
+            "</td><td>" +
+            res[i]["Visitante"] +
+            "</td></tr>"
+        );
+      }
+    },
+  });
+
+  $.ajax({
+    url: "../Telematica/php/Partidos2.php",
+    type: "GET",
+    dataType: "JSON",
+    data: {}, //this is data you send to your server
+    success: function (res) {
+      console.log(res);
+      console.log(res.length);
+
+      for (i = 0; i < res.length; i++) {
+        $("#Partido2").append(
+          "<tr><td>" +
+            res[i]["Local"] +
+            "</td><td>" +
+            res[i]["Visitante"] +
+            "</td></tr>"
+        );
+      }
+    },
+  });
+
+  $.ajax({
+    url: "../Telematica/php/Partidos3.php",
+    type: "GET",
+    dataType: "JSON",
+    data: {}, //this is data you send to your server
+    success: function (res) {
+      console.log(res);
+      console.log(res.length);
+
+      for (i = 0; i < res.length; i++) {
+        $("#Partido3").append(
+          "<tr><td>" +
+            res[i]["Local"] +
+            "</td><td>" +
+            res[i]["Visitante"] +
+            "</td></tr>"
+        );
+      }
+    },
+  });
+
+  $.ajax({
+    url: "../Telematica/php/Partidos4.php",
+    type: "GET",
+    dataType: "JSON",
+    data: {}, //this is data you send to your server
+    success: function (res) {
+      console.log(res);
+      console.log(res.length);
+
+      for (i = 0; i < res.length; i++) {
+        $("#Partido4").append(
+          "<tr><td>" +
+            res[i]["Local"] +
+            "</td><td>" +
+            res[i]["Visitante"] +
+            "</td></tr>"
+        );
+      }
+    },
+  });
 }
 table();
---
+++ php/Partidos2.php
@@ -0,0 +1,16 @@
+<?php
+include 'varia.php';
+$temparray = array();
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
+$result = mysqli_query($conn, "SELECT (SELECT equipo FROM Final.Partidos1, Final.Equipos  WHERE id_local=id_Equipos && id_Partido = 2) as Local, (SELECT equipo FROM Final.Partidos1, Final.Equipos  WHERE id_Visitante=id_Equipos && id_Partido = 2) as Visitante 
+FROM Final.Partidos1, Final.Equipos
+ORDER BY Local  desc LIMIT 1"); // genera el query a SQL
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+        array_push($temparray,$row);
+    }
+    
+}
+
+echo json_encode($temparray);
+?>
